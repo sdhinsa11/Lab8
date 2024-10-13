9f554d29b31a482fd6959cef1747d096c322f509
SDHINSA <sohanad@icloud.com>
2024-10-12 21:32:21 -0600

    Second Test Case

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,17 +46,22 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     /**
-     * this gets size of the list * @return
+     * this gets size of the list *
+     * @return
      */
     public int getCount(){
         return cities.size();
     }
     /**
-     * this adds a city object to the list *for the first phase it will be empty * @param city
+     * this adds a city object to the list *the second phase,
+     * you can add the city *
+     * @param city
      */
-    public void addCity(City city){
-
+    public void addCity(City city)
+    {
+        cities.add(city);
     }
+
 
 
 }
